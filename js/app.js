ef876899f652db1641c9ff91df076d8cdf4e421d
Defne Tunçer <defnetuncer@hacettepe.edu.tr>
2019-07-21 11:18:17 -0400

    Estimated Step Count is added

--- js/app.js
+++ js/app.js
@@ -1,25 +1,34 @@
-const margin = { top: 80, right: 40, bottom: 40, left: 200 };
-const width = 600 - margin.right - margin.left;
-const height = 500 - margin.top - margin.bottom;
 
-const svg = d3
-    .select('.vizGoogleFit')
-    .append('svg')
-    .attr('width', width + margin.right + margin.left)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-var apiKey = 'AIzaSyCuvY16_caGg_Yq2JPIFSIHhbcMZC0keIs';
+function viz(data){
+    console.log(data);
+    console.log((data.map(d => d.value[0].intVal)));
+    var sum = d3.sum(data.map(d => d.value[0].intVal));
+    var auth = auth2.currentUser.get().getBasicProfile();
+
+    d3
+    .select('.auth-info')
+    .attr('style','display:block;color:gray;')
+    .text(auth.getGivenName() + ' ' + auth.getFamilyName());
+    
+    d3
+    .select('.daily-step')
+    .attr('style','display:block;color:gray;')
+    .text(sum + ' steps taken today');
+}
+
+
+var apiKey = 'AIzaSyAFE7Ispkhhy9ZR68mzTL6TWFMrJufkWfU';
 var clientId = '283704614465-a6octk36hj6inh564f23d2t10mt0u9fr.apps.googleusercontent.com';
 var scopes = 'https://www.googleapis.com/auth/fitness.activity.read';
 var auth2;
 var authorizeButton = document.getElementById('authorize-button');
 var signoutButton = document.getElementById('signout-button');
+
 function handleClientLoad() {
-  // Load the API client and auth library
     gapi.load('client:auth2', initAuth);
 }
+
 function initAuth() {
     gapi.client.setApiKey(apiKey);
     gapi.auth2.init({
@@ -27,14 +36,13 @@ function initAuth() {
         scope: scopes
     }).then(function () {
         auth2 = gapi.auth2.getAuthInstance();
-        // Listen for sign-in state changes.
         auth2.isSignedIn.listen(updateSigninStatus);
-        // Handle the initial sign-in state.
         updateSigninStatus(auth2.isSignedIn.get());
         authorizeButton.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
       });
-    }
+}
+
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
         authorizeButton.style.display = 'none';
@@ -45,24 +53,27 @@ function updateSigninStatus(isSignedIn) {
         signoutButton.style.display = 'none';
     }
 }
+
 function handleAuthClick(event) {
     auth2.signIn();
 }
+
 function handleSignoutClick(event) {
     auth2.signOut();
+    d3.select('.daily-step').attr('style','display:none;');
+    d3.select('.auth-info').attr('style','display:none;');
 }
-// Load the API and make an API call.
+
 function makeApiCall() {
+    var date = new Date().getTime();
     gapi.client.load('fitness', 'v1', function() {
         var request = gapi.client.fitness.users.dataSources.datasets.get({
             userId: 'me',
             dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
-            datasetId: '1476092378000000-' + new Date().getTime() + '000000',
+            datasetId: date-86400000 + '000000-' + date + '000000',
         });
         request.execute(function(resp) {
-          console.log(resp);
+            viz(resp.point);
         });
     });
-
-    console.log(auth2.currentUser.get().getBasicProfile().getGivenName());
-    }
+}
