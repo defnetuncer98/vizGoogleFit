be0008e87a195700082993fc73015137af695932
Defne Tunçer <defnetuncer@hacettepe.edu.tr>
2019-07-24 11:22:45 -0400

    Slider is added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-function viz(data){
-    var days = [];
+var days = [];
+var apiKey = 'AIzaSyAFE7Ispkhhy9ZR68mzTL6TWFMrJufkWfU';
+var clientId = '283704614465-a6octk36hj6inh564f23d2t10mt0u9fr.apps.googleusercontent.com';
+var scopes = 'https://www.googleapis.com/auth/fitness.activity.read';
+var auth2;
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+var stepsButton = document.getElementById('steps-button');
+var caloriesButton = document.getElementById('calories-button');
+var midnight = new Date().setHours(0,0,0,0);
+var dailystep;
+
+function prepareData(data){
     var sum = 0;
     var dayends = 0;
     data.forEach(function(element) {
@@ -13,53 +24,78 @@ function viz(data){
         }
     });
     days.push(sum);
+    days.push(dailystep);
+}
 
+function viz(max){
+    if(max>days.length) {
+        data = days;
+        d3.select(".header").text("Daily Steps | Last " + days.length + " Days");
+    }
+    else{
+        data = days.slice(days.length-max, days.length);
+        d3.select(".header").text("Daily Steps | Last " + max + " Days");
+    }
     offsetwidth = document.getElementById("vizGoogleFit").offsetWidth;
 
     var xScale = d3.scaleLinear()
-        .domain([1, days.length]) // input
+        .domain([0, data.length]) // input
         .range([0, offsetwidth]); // output
 
     var yScale = d3.scaleLinear()
-        .domain([0, Math.max(...days)]) // input 
+        .domain([0, Math.max(...data)]) // input 
         .range([window.innerHeight/2, 0]); // output 
 
-    console.log(days);
-
+    console.log(data);
+    
+    d3.select('.svg').remove();
+    d3.select('.slider').attr('style','display:block;');
     var svg = d3.select(".vizGoogleFit").append("svg")
+    .attr("class","svg")
     .attr("width", offsetwidth)
     .attr("height", window.innerHeight/2)
-    .attr("style","margin:20px;")
     .append("g");
 
     svg.append("path")
-    .datum(days) // 10. Binds data to the line 
+    .datum(data)
     .attr("class", "line") // Assign a class for styling 
     .style('fill', 'none')
     .attr("stroke", "purple")
     .attr("stroke-width", 1.5)
     .attr("d", d3.line()
     .x(function(d, i) { return xScale(i); })
-    .y(function(d) {return yScale(d); }));
+    .y(function(d) {return yScale(d); })
+    .curve(d3.curveMonotoneX));
 
-    offsetheight = document.getElementById("vizGoogleFit").offsetHeight;
-    debugger;
-    svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + offsetheight + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+    var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("color","white");
 
-    svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+    svg.selectAll("circle")
+    .data(data)
+    .enter()    
+    .append("svg:circle")
+    .attr("cx", function(d, i) { return xScale(i) })
+    .attr("cy", function(d) { return yScale(d) })
+    .attr("r", 3)
+    .attr("style","fill:indigo;stroke:indigo")
+    .on("mouseover", function(a, b, c) {
+        d3.select(this).attr("r", "6");
+        tooltip.text(a);
+        return tooltip.style("visibility", "visible");
+    })
+    .on("mousemove", function(){
+        return tooltip.style("top",(d3.event.pageY-30)+"px").style("left",(d3.event.pageX-5)+"px");
+    })
+    .on("mouseout", function() { 
+        d3.select(this).attr("r", "3")
+        return tooltip.style("visibility", "hidden");
+    });
+
 }
-var apiKey = 'AIzaSyAFE7Ispkhhy9ZR68mzTL6TWFMrJufkWfU';
-var clientId = '283704614465-a6octk36hj6inh564f23d2t10mt0u9fr.apps.googleusercontent.com';
-var scopes = 'https://www.googleapis.com/auth/fitness.activity.read';
-var auth2;
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-var stepsButton = document.getElementById('steps-button');
 
 function handleClientLoad() {
     gapi.load('client:auth2', initAuth);
@@ -77,6 +113,7 @@ function initAuth() {
         authorizeButton.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
         stepsButton.onclick = handleStepsClick;
+        caloriesButton.onclick = handleCaloriesClick;
       });
 }
 
@@ -89,12 +126,17 @@ function updateSigninStatus(isSignedIn) {
         d3.select('.auth-image').attr('style','display:block;padding:12px;').attr('src',auth.getImageUrl());
         d3.select('.auth-info').text(auth.getGivenName() + ' ' + auth.getFamilyName());
         d3.select('.steps-button').attr('style','display:block;');
+        d3.select('.calories-button').attr('style','display:block;');
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
         d3.select('.auth-image').attr('style','display:none;');
         d3.select('.daily-step').attr('style','display:none;');
         d3.select('.auth-info').text('Get Started by Connecting your Google Account!');
+        d3.select('.steps-button').attr('style','display:none;');
+        d3.select('.svg').remove();
+        d3.select('.slider').attr('style','display:none;');
+        d3.select('.header').text('');
     }
 }
 
@@ -107,38 +149,43 @@ function handleSignoutClick(event) {
 }
 
 function handleStepsClick(event) {
+    d3.select('.calories-button').attr('style','background-color:#e4e4e4;color:black;');
     d3.select('.steps-button').attr('style','background-color:purple;color:white;');
     makeApiCallSteps();
 }
 
+function handleCaloriesClick(event) {
+    d3.select('.calories-button').attr('style','background-color:indigo;color:white;');
+    d3.select('.steps-button').attr('style','background-color:#e4e4e4;color:black;');
+}
+
 function makeApiCallSteps() {
-    var date = new Date().getTime();
     gapi.client.load('fitness', 'v1', function() {
         var request = gapi.client.fitness.users.dataSources.datasets.get({
             userId: 'me',
             dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
-            datasetId: date-86400000*7 + '000000-' + date + '000000',
+            datasetId: midnight-86400000*100 + '000000-' + midnight + '000000',
         });
         request.execute(function(resp) {
-            viz(resp.point);
+            prepareData(resp.point);
+            viz(7);
         });
     });
 }
 
 function makeApiCallDailySteps() {
-    var date = new Date().getTime();
     gapi.client.load('fitness', 'v1', function() {
         var request = gapi.client.fitness.users.dataSources.datasets.get({
             userId: 'me',
             dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps',
-            datasetId: date-86400000 + '000000-' + date + '000000',
+            datasetId: midnight + '000000-' +  new Date().getTime() + '000000',
         });
         request.execute(function(resp) {
-            var sum = d3.sum(resp.point.map(d => d.value[0].intVal));
+            dailystep = d3.sum(resp.point.map(d => d.value[0].intVal));
             d3
             .select('.daily-step')
             .attr('style','display:block;color:white;')
-            .text(sum);
+            .text(dailystep);
         });
     });
 }
